cda60e921612eb3ced16f6352fa7a03c8d151941
MHamzazai <farhatifraq@gmail.com>
2024-11-12 00:29:14 +0500

    first commit

---
+++ src/app/api/fetch-rates/route.ts
@@ -0,0 +1,19 @@
+import axios from "axios"
+import { NextResponse } from "next/server"; // used for handling server side response
+
+export async function GET() {
+    try {
+        // my coinlayer api
+        const myKey: string = "7166a9ac293065cdd780c5459f91dc12";
+
+        // making an api call 
+        // destructuring data constant
+        const { data } = await axios.get(`https://api.coinlayer.com/api/live?access_key=${myKey}`);
+        // if the data is fetch successfully 
+        return NextResponse.json(data);
+    }
+    catch (error) {
+        // if the data is not fetch
+        return NextResponse.json({ error: "Unable to fetch data!" }, { status: 500 });
+    }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,118 @@
-import Image from "next/image";
+'use client'; // because react hooks are only work in CSR;
+
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  // making a type for allRates constant so that typescript understand it 
+  type AllRatesType = { [key: string]: number };
+
+  // making states for different conditions
+  const [allRates, setAllRates] = useState<AllRatesType>({}); // for storing all the currency rates
+  const [inputCurrency, setInputCurrency] = useState(''); // for storing the ussr input currency name
+  const [inputRate, setInputRate] = useState<number | null>(null); // for storing the currency rate that the user enter
+  const [error, setError] = useState('');
+
+  // using useEffect hook to fetch currency rates after component mounts
+  useEffect(() => {
+    // making a function which fetches the rates from data
+    const fetchRates = async () => {
+      try {
+        // making an api call to coinlayer and destructuring the constant
+        const { data } = await axios.get("api/fetch-rates");
+        // stores all the rates in the allRates constant
+        setAllRates(data.rates);
+      } catch {
+        setError("Unable to fetch rates !");
+      }
+    }
+    // calling the function to fetch rates
+    fetchRates();
+  }, []); // "[]" for mount the component at onece
+
+  // clearing the inputRate and setError after inputCurrency changes
+  useEffect(() => {
+    setInputRate(null);
+    setError('');
+  }, [inputCurrency]);
+
+  // making another function to handle user unput currency
+  const handleUserInput = () => {
+    if (inputCurrency && allRates[inputCurrency.toUpperCase()]) {
+      setInputRate(allRates[inputCurrency.toUpperCase()]);
+      setError('');
+      { }
+    }
+    else {
+      setInputRate(null);
+      setError("Currency not found !");
+    }
+  }
+
+  return (
+    <div className="w-full h-full">
+
+      <nav className="w-full h-40 overflow-hidden bg-slate-100 flex md:justify-between items-center shadow-black shadow-sm md:shadow-lg mb-4">
+
+        <div className="hidden md:inline-block">
+          <Link href="" className="xl:ml-5 xl:text-3xl ml-2  text-xl font-semibold capitalize tracking-wide
+        hover:bg-black hover:text-white hover:scale-105 hover:px-3 hover:py-1 hover:rounded-md transition-all">Go to GitHub</Link>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="text-center md:h-full md:w-56 md:pt-3 xl:h-full xl:w-80 xl:pt-6 2xl:w-1/2 2xl:text-4xl text-3xl font-bold font-sans capitalize xl:border-b-4 border-black tracking-wider hover:border-0 hover:border-x-2 
+        hover:px-2 hover:border-slate-300 hover:rounded-md hover:text-slate-700 hover:scale-105 hover:bg-slate-50 hover:py-2
+        transition-all">
+          <span className="text-3xl 2xl:text-5xl font-semibold xl:px-1 text-slate-600">welcome</span> to check currency rates
+          <span className="text-3xl 2xl:text-5xl font-semibold xl:px-1 text-slate-600"> website</span>
+        </div>
+
+        <div className="hidden md:inline-block">
+          <Link href="" className="text-xl xl:text-3xl mr-2 xl:mr-5 capitalize font-semibold tracking-wide
+          hover:bg-black hover:text-white hover:scale-105 hover:px-3 hover:py-1 hover:rounded-md transition-all">Got to Npm</Link>
+        </div>
+
+      </nav>
+
+      <div className="w-full h-100 overflow-hidden bg-slate-50 mt-16">
+        <h1 className="text-4xl capitalize font-sans px-1 py-5 font-bold">here you can find</h1>
+        <p className="text-2xl capitalize px-3 font-semibold mb-5">different types of <strong className="px-1">latest</strong> currency rates in the world.</p>
+      </div>
+
+      <div className="w-full h-[50%] my-5 flex justify-center">
+        <div className="w-[100%] xl:w-[50%] h-full bg-slate-200 rounded-md border-black">
+          <h1 className="text-center text-4xl my-4 font-bold border-b-2 border-black tracking-wide capitalize py-3">
+            check currency rate
+          </h1>
+
+          <label htmlFor="currencyName" className="block text-3xl py-4 text-center capitalize">enter Currency Name</label>
+          <input
+            type="text"
+            name="name"
+            value={inputCurrency}
+            onChange={(e) => setInputCurrency(e.target.value)}
+            placeholder="Enter Here"
+            id="currencyName"
+            className="block px-2 w-4/5 border-2 xl:w-1/2 border-black py-1 rounded-sm text-center mx-auto text-xl" />
+
+          <div className="text-center">
+            <button className="bg-red-300 px-5 py-0 capitalize text-center my-5 text-2xl rounded-md md:rounded-sm font-semibold
+              hover:bg-black hover:text-white hover:scale-105 hover:px-6 hover:py-1 hover:rounded-md transition-all
+               hover:text-center" onClick={handleUserInput}>
+              check Rate
+            </button>
+          </div>
+          {inputRate !== null &&
+            <div className="text-center mt-3 transition-all pb-2">
+              <p className="text-2xl font-semibold">Currency <span> '{inputCurrency.toUpperCase()}' </span>
+                Rate: '{Math.round(inputRate)}'<sub>{inputCurrency.toUpperCase()}</sub>
+              </p>
+            </div>
+          }
+          {error && <div className="text-center text-red-500 text-2xl mt-3 capitalize transition-all pb-2">{error}</div>}
+        </div>
+      </div>
     </div>
   );
 }
